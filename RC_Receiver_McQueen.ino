a5e59c831af8a444925d8c48f1295dc54f415cd1
TheDIYGuy999 <msommer74@gmail.com>
2016-08-06 16:03:49 +0200

    Backchannel added

--- RC_Receiver_McQueen.ino
+++ RC_Receiver_McQueen.ino
@@ -40,6 +40,9 @@ const int maxVehicleNumber = 5;
 // the ID number of the used "radio pipe" must match with the selected ID on the transmitter!
 const uint64_t pipeIn[maxVehicleNumber] = { 0xE9E8F0F0B1LL, 0xE9E8F0F0B2LL, 0xE9E8F0F0B3LL, 0xE9E8F0F0B4LL, 0xE9E8F0F0B5LL };
 
+// Hardware configuration: Set up nRF24L01 radio on hardware SPI bus & pins A0 (CE) & A1 (CSN)
+RF24 radio(A0, A1);
+
 // The size of this struct should not exceed 32 bytes
 struct RcData {
   byte axis1; // Aileron (Steering for car)
@@ -49,20 +52,24 @@ struct RcData {
   boolean mode1 = false; // Speed limitation
   boolean mode2 = false;
 };
-
 RcData data;
 
-// Hardware configuration: Set up nRF24L01 radio on hardware SPI bus & pins A0 (CE) & A1 (CSN)
-RF24 radio(A0, A1);
+// This struct defines data, which are embedded inside the ACK payload
+struct ackPayload {
+  float vcc; // vehicle vcc voltage
+  float batteryVoltage; // vehicle battery voltage
+  boolean batteryOk; // the vehicle battery voltage is OK!
+};
+ackPayload payload;
 
-// Battery
-boolean batteryOK = false;
+// Battery voltage detection pin
+#define BATTERY_DETECT_PIN A3 // The 10k & 10k battery detection voltage divider is connected to pin A3
 
 // Create Servo object
 Servo steeringServo;
 
 // Status LED objects
-statusLED battLED(true); // true = inversed!
+statusLED battLED(true); // true = inversed! (wired from pin to vcc)
 
 // Timer
 SimpleTimer timer;
@@ -73,7 +80,7 @@ SimpleTimer timer;
 // NOTE: The first pin must always be PWM capable, the second only, if the last parameter is set to "true"
 // SYNTAX: IN1, IN2, min. input value, max. input value, neutral position width
 // invert rotation direction, true = both pins are PWM capable
-DRV8833 Motor(motor_in1, motor_in2, 0, 100, 6, false, true);
+DRV8833 Motor(motor_in1, motor_in2, 0, 100, 2, false, true);
 
 //
 // =======================================================================================================
@@ -98,8 +105,10 @@ void setup() {
   radio.setPALevel(RF24_PA_HIGH);
   radio.setDataRate(RF24_250KBPS);
   radio.setAutoAck(true);                  // Ensure autoACK is enabled
-  radio.setRetries(1, 5);                  // 1x250us delay (blocking!!), max. 5 retries
-  radio.setCRCLength(RF24_CRC_8);          // Use 8-bit CRC for performance
+  radio.enableAckPayload();
+  radio.enableDynamicPayloads();
+  radio.setRetries(5, 5);                  // 5x250us delay (blocking!!), max. 5 retries
+  //radio.setCRCLength(RF24_CRC_8);          // Use 8-bit CRC for performance
 
 #ifdef DEBUG
   radio.printDetails();
@@ -122,7 +131,7 @@ void setup() {
   data.axis3 = 50;
   data.axis4 = 50;
 
-  timer.setInterval(100, checkBattery); // Check battery voltage every 100ms
+  timer.setInterval(1000, checkBattery); // Check battery voltage every 100ms
 }
 
 //
@@ -133,16 +142,15 @@ void setup() {
 
 void led() {
 
-  // NOTE: The LED 17 (RX) is inversed! OFF means ON etc.!!!!*******************
+  // NOTE: The LED 17 (RX) is used
 
   //batteryOK = false;
 
   // Red LED (ON = battery empty, blinking = OK
-  if (batteryOK) {
+  if (payload.batteryOk) {
     battLED.flash(140, 150, 500, vehicleNumber); // ON, OFF, PAUSE, PULSES
-    //battLED.flash(100, 30, 0, 0); // ON, OFF, PAUSE, PULSES
   } else {
-    battLED.off(); // Always ON = battery low voltage
+    battLED.on(); // Always ON = battery low voltage
   }
 }
 
@@ -155,9 +163,11 @@ void led() {
 void readRadio() {
 
   static unsigned long lastRecvTime = 0;
+  byte pipeNo;
 
-  if (radio.available()) {
-    radio.read(&data, sizeof(RcData));
+  if (radio.available(&pipeNo)) {
+    radio.writeAckPayload(pipeNo, &payload, sizeof(struct ackPayload) );  // prepare the ACK payload
+    radio.read(&data, sizeof(struct RcData)); // read the radia data and send out the ACK payload
     lastRecvTime = millis();
 #ifdef DEBUG
     Serial.print(data.axis1);
@@ -171,7 +181,7 @@ void readRadio() {
 #endif
   }
 
-  if (millis() - lastRecvTime > 1000) { // bring all servos to their middle position, if no RC signal is received for 1s!
+  if (millis() - lastRecvTime > 1000) { // bring all servos to their middle position, if no RC signal is received during 1s!
     data.axis1 = 50; // Aileron (Steering for car)
     data.axis2 = 50; // Elevator
     data.axis3 = 50; // Throttle
@@ -189,7 +199,7 @@ void readRadio() {
 //
 
 void writeSteeringServo() {
-  steeringServo.write(map(data.axis1, 100, 0, 68, 101) ); // 0 - 100% = 86 - 105° ( 90% is the center in theory) R - L
+  steeringServo.write(map(data.axis1, 100, 0, 68, 101) ); // 0 - 100% = 86 - 105° ( 90° is the center in theory) R - L
 }
 
 //
@@ -202,7 +212,7 @@ void driveMotor() {
 
   int maxPWM;
   int maxAcceleration;
-  
+
   // Speed limitation (max. is 255)
   if (data.mode1) {
     maxPWM = 170; // Limited
@@ -216,7 +226,7 @@ void driveMotor() {
   } else {
     maxAcceleration = 7; // Full
   }
-  
+
   // ***************** Note! The ramptime is intended to protect the gearbox! *******************
   // SYNTAX: Input value, max PWM, ramptime in ms per 1 PWM increment
   // true = brake active, false = brake in neutral position inactive
@@ -225,23 +235,25 @@ void driveMotor() {
 
 //
 // =======================================================================================================
-// CHECK BATTERY VOLTAGE
+// CHECK RX BATTERY & VCC VOLTAGES
 // =======================================================================================================
 //
 
 void checkBattery() {
-  float batteryVolt = readVcc() / 1000.0 ;
 
-  if (batteryVolt >= 3.0) {
-    batteryOK = true;
+  payload.batteryVoltage = analogRead(BATTERY_DETECT_PIN) / 155.15; // 1023steps / 6.6V = 155.15
+  payload.vcc = readVcc() / 1000.0;
+
+  if (payload.batteryVoltage >= 3.5) {
+    payload.batteryOk = true;
 #ifdef DEBUG
-    Serial.print(batteryVolt);
+    Serial.print(payload.batteryVoltage);
     Serial.println(" V. Battery OK");
 #endif
   } else {
-    batteryOK = false;
+    payload.batteryOk = false;
 #ifdef DEBUG
-    Serial.print(batteryVolt);
+    Serial.print(payload.batteryVoltage);
     Serial.println(" V. Battery empty!");
 #endif
   }
